4ce35b90595a29ec2c43df241e9ee762df1f48cd
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-07-17 15:49:28 +0400

    Update On the Class.

--- src/main/java/com/gameApi/gameapi/PlayerController.java
+++ src/main/java/com/gameApi/gameapi/PlayerController.java
@@ -1,10 +1,7 @@
 package com.gameApi.gameapi;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -14,7 +11,7 @@ public class PlayerController {
 
     CopyOnWriteArrayList<Player> listOfPlayers = new CopyOnWriteArrayList<>();
     @PostMapping
-    public Player creatPlayer(Player incomingPlayer){
+    public Player creatPlayer(@RequestBody Player incomingPlayer){
       //  Player newPlayer = new Player();
         listOfPlayers.add(incomingPlayer);
         return incomingPlayer;
