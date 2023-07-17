4ba22b7a669e7f9a6a31f38fb4e317f8d5f65e87
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-07-17 15:35:23 +0400

    Update On the Class.

--- src/main/java/com/gameApi/gameapi/Player.java
+++ src/main/java/com/gameApi/gameapi/Player.java
@@ -1,17 +1,8 @@
 package com.gameApi.gameapi;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/api/v1/player")
 public class Player {
-    @PostMapping
-    public String yetAnotherMessage(){
-        return "     ";
-    }
+    public String id;
+    public String name;
 
 }
